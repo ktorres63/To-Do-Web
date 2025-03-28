52671ec6c19fe1aa4fd94d77dbdab9519d1105c3
ktorres63 <ktorresaro@unsa.edu.pe>
2025-03-27 22:29:13 -0500

    Add dialog to create projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./styles.css";
 import { toggleTheme } from "./modules/theme.js";
+
 document.getElementById("toggleTheme").addEventListener("click", toggleTheme);
+
+document.getElementById("menu-btn").addEventListener("click", function () {
+  const aside = document.getElementById("aside-menu");
+  aside.classList.toggle("hidden");
+});
+
+const dialog = document.getElementById("project-dialog");
+const openDialogBtn = document.getElementById("add-project");
+const cancelBtn = document.getElementById("cancel-btn");
+
+openDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+cancelBtn.addEventListener("click", () => {
+  dialog.close();
+});
---
+++ src/modules/ui.js
@@ -0,0 +1,24 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const menuBtn = document.getElementById("menu-btn");
+//   const aside = document.getElementById("aside-menu");
+
+//   if (menuBtn && aside) {
+//     menuBtn.addEventListener("click", function () {
+//       aside.classList.toggle("hidden");
+//     });
+//   } else {
+//     console.error("El botón o el aside no existen en el DOM.");
+//   }
+// });
+
+// const dialog = document.getElementById("projectDialog");
+// const openDialogBtn = document.getElementById("add-project");
+// const cancelBtn = document.getElementById("cancelBtn");
+
+// openDialogBtn.addEventListener("click", () => {
+//   dialog.showModal();
+// });
+
+// cancelBtn.addEventListener("click", () => {
+//   dialog.close();
+// });
